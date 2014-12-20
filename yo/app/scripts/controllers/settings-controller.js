5a4856df10d9f75781b2dafaac3af7cafa86dac3
Daniel Noguerol <dan@whizzosoftware.com>
2014-12-20 01:10:29 -0700

    Fixed issue with saving log setting when no other settings fields were populated.

--- yo/app/scripts/controllers/settings-controller.js
+++ yo/app/scripts/controllers/settings-controller.js
@@ -9,9 +9,11 @@ angular.module('hobsonApp').
             var setConfiguration = function(result) {
                 console.debug('configuration = ', result.data);
                 $scope.name = result.data.name;
-                $scope.address = result.data.location.text;
-                $scope.latitude = result.data.location.latitude;
-                $scope.longitude = result.data.location.longitude;
+                if (result.data.location) {
+                  $scope.address = result.data.location.text;
+                  $scope.latitude = result.data.location.latitude;
+                  $scope.longitude = result.data.location.longitude;
+                }
                 $scope.logLevel = result.data.logLevel;
             };
 
@@ -34,14 +36,30 @@ angular.module('hobsonApp').
 
             $scope.save = function() {
                 var config = {
-                  name: $scope.name,
-                  location: {
-                    text: ($scope.address === '') ? null : $scope.address,
-                    latitude: ($scope.latitude === '') ? null : parseFloat($scope.latitude),
-                    longitude: ($scope.longitude === '') ? null : parseFloat($scope.longitude)
-                  },
                   logLevel: $scope.logLevel
                 };
+
+                if ($scope.name && $scope.name !== '') {
+                  config.name = $scope.name;
+                }
+                if ($scope.address && $scope.address !== '') {
+                  if (!config.location) {
+                    config.location = {};
+                  }
+                  config.location.text = $scope.address;
+                }
+                if ($scope.latitude && $scope.latitude !== '') {
+                  if (!config.location) {
+                    config.location = {};
+                  }
+                  config.location.latitude = parseFloat($scope.latitude);
+                }
+                if ($scope.longitude && $scope.longitude !== '') {
+                  if (!config.location) {
+                    config.location = {};
+                  }
+                  config.location.longitude = parseFloat($scope.longitude);
+                }
 
                 SettingsService.setConfiguration(config).then(function() {
                   toastr.success('The settings have been saved.', null, {
