6563bf49ca4e002e710b12efcaf03b910016c64c
Daniel Noguerol <dan@whizzosoftware.com>
2015-01-22 16:00:23 -0700

    Implemented HOBWCN-8 - The camera image dialog should indicate authentication failures. Implemented HOBWCN-9 - A loading indicator should be shown when viewing a camera image.

--- yo/app/scripts/controllers/video-view-dialog-controller.js
+++ yo/app/scripts/controllers/video-view-dialog-controller.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('hobsonApp').
-    controller('VideoViewDialogController', ['$scope', 'AppData', 'DialogContextService',
-        function($scope, AppData, DialogContextService) {
+    controller('VideoViewDialogController', ['$scope', '$http', 'AppData', 'DialogContextService',
+        function($scope, $http, AppData, DialogContextService) {
             $scope.deviceName = DialogContextService.getParams().deviceName;
-            $scope.videoUrl = DialogContextService.getParams().videoUrl;
+            var videoUrl = DialogContextService.getParams().videoUrl;
+            $http.head(videoUrl).success(function() {
+              $scope.videoUrl = videoUrl;
+            }).error(function(data, status, headers, config) {
+              if (status === 401) {
+                $scope.error = 'Hobson is not authorized to access this device\'s stream. Please make sure you have entered the correct username and password in the device\'s settings.';
+              } else {
+                $scope.error = 'An error occurred accessing this device\'s stream. Please see the log for details.';
+              }
+            });
 
             $scope.close = function() {
                 DialogContextService.currentModalInstance().dismiss();
