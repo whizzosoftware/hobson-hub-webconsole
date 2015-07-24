24df73d6cb526263cbf8c9189d5bc85010219622
Daniel Noguerol <dan@whizzosoftware.com>
2015-07-24 13:19:28 -0600

    Added redirection handling for /console and /console/

--- src/main/java/com/whizzosoftware/hobson/webconsole/WebConsoleApplication.java
+++ src/main/java/com/whizzosoftware/hobson/webconsole/WebConsoleApplication.java
@@ -1,7 +1,11 @@
 package com.whizzosoftware.hobson.webconsole;
 
 import org.restlet.Application;
+import org.restlet.Request;
+import org.restlet.Response;
 import org.restlet.Restlet;
+import org.restlet.data.Reference;
+import org.restlet.routing.Redirector;
 import org.restlet.routing.Router;
 
 public class WebConsoleApplication extends Application {
@@ -10,5 +14,15 @@ public class WebConsoleApplication extends Application {
         Router router = new Router();
         router.attach("/", new ClassLoaderOverrideDirectory(getContext(), "clap://class/www/", getClass().getClassLoader()));
         return router;
+    }
+
+    @Override
+    public void handle(Request request, Response response) {
+        Reference ref = request.getResourceRef();
+        if ("/console".equals(ref.getPath()) || "/console/".equals(ref.getPath())) {
+            response.redirectPermanent("/console/index.html");
+        } else {
+            super.handle(request, response);
+        }
     }
 }
