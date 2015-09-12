dddb5bef5d62200bdb07446802d8c632773e64e3
Daniel Noguerol <dan@whizzosoftware.com>
2015-09-11 22:37:58 -0600

    Made karma tests a single run.

--- yo/test/js/colorConversion.js
+++
@@ -1,7 +0,0 @@
-define(['app/js/services/colorConversion.js'], function(colorConversion) {
-	describe('colorConversion', function() {
-		it('should convert rgb values', function() {
-			console.debug(colorConversion.covertRgbToBytes('rgb(255,255,255)'));
-		});
-	});
-});
