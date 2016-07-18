b385d51ba83282d1b0f7db2e81ca3088fd902adb
Daniel Noguerol <dan@whizzosoftware.com>
2016-07-18 09:57:21 -0600

    Now shows error highlighting around person and location combo boxes.

--- yo/app/js/hub/views/widgets/locationPicker.js
+++ yo/app/js/hub/views/widgets/locationPicker.js
@@ -1,53 +1,66 @@
 // Filename: views/config/locationPicker.js
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'toastr',
-	'services/hub',
-	'views/widgets/baseWidget',
-	'i18n!nls/strings',
-	'text!templates/widgets/locationPicker.html'
-], function($, _, Backbone, toastr, HubService, BaseWidget, strings, pluginConfigTemplate) {
+  'jquery',
+  'underscore',
+  'backbone',
+  'toastr',
+  'services/hub',
+  'views/widgets/baseWidget',
+  'i18n!nls/strings',
+  'text!templates/widgets/locationPicker.html'
+], function ($, _, Backbone, toastr, HubService, BaseWidget, strings, pluginConfigTemplate) {
 
-	return BaseWidget.extend({
+  return BaseWidget.extend({
 
-		template: _.template(pluginConfigTemplate),
+    template: _.template(pluginConfigTemplate),
 
-		initialize: function(options) {
-			this.required = this.model && this.model.constraints ? this.model.constraints.required : false;
-			this.value = options.value;
-		},
+    initialize: function (options) {
+      this.required = this.model && this.model.constraints ? this.model.constraints.required : false;
+      this.value = options.value;
+    },
 
-		render: function() {
-			this.$el.html(this.template({
-				strings: strings,
-				id: this.getSafeId(),
-				property: this.model,
-				required: this.required,
-				value: this.value
-			}));
+    render: function () {
+      this.$el.html(this.template({
+        strings: strings,
+        id: this.getSafeId(),
+        property: this.model,
+        required: this.required,
+        value: this.value
+      }));
 
-			// load locations
-			HubService.getLocations(this, function(model, response, options) {
-				var el = options.context.$el.find('#' + options.context.getSafeId());
-				el.empty();
-				for (var ix=0; ix < model.length; ix++) {
-					el.append('<option value="' + model.at(ix).get('@id') + '">' + model.at(ix).get('name') + '</option>');
-				}
-				el.removeAttr('disabled');
-			}, function(model, response, options) {
-				toastr.error('Error retrieving location list');
-			});
+      // load locations
+      HubService.getLocations(this, function (model, response, options) {
+        var el = options.context.$el.find('#' + options.context.getSafeId());
+        el.empty();
+        for (var ix = 0; ix < model.length; ix++) {
+          el.append('<option value="' + model.at(ix).get('@id') + '">' + model.at(ix).get('name') + '</option>');
+        }
+        el.removeAttr('disabled');
+      }, function (model, response, options) {
+        toastr.error('Error retrieving location list');
+      });
 
-			return this;
-		},
+      return this;
+    },
 
-	    getValue: function() {
-	    	var id = this.$el.find('#' + this.getSafeId()).val();
-	    	return {'@id': id, name: this.$el.find('option[value="' + id + '"]').text()};
-	    }
+    getValue: function () {
+      var id = this.$el.find('#' + this.getSafeId()).val();
+      if (id) {
+        return {'@id': id, name: this.$el.find('option[value="' + id + '"]').text()};
+      } else {
+        return null;
+      }
+    },
 
-	});
+    showError: function (showError) {
+      BaseWidget.prototype.showError.call(this, showError);
+      if (showError) {
+        this.$el.find('#location').addClass('error');
+      } else {
+        this.$el.find('#location').removeClass('error');
+      }
+    }
+
+  });
 
 });
--- yo/app/js/hub/views/widgets/presenceEntityPicker.js
+++ yo/app/js/hub/views/widgets/presenceEntityPicker.js
@@ -1,52 +1,65 @@
 // Filename: views/config/presenceEntityPicker.js
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'services/hub',
-	'views/widgets/baseWidget',
-	'i18n!nls/strings',
-	'text!templates/widgets/presenceEntityPicker.html'
-], function($, _, Backbone, HubService, BaseWidget, strings, pluginConfigTemplate) {
+  'jquery',
+  'underscore',
+  'backbone',
+  'services/hub',
+  'views/widgets/baseWidget',
+  'i18n!nls/strings',
+  'text!templates/widgets/presenceEntityPicker.html'
+], function ($, _, Backbone, HubService, BaseWidget, strings, pluginConfigTemplate) {
 
-	return BaseWidget.extend({
+  return BaseWidget.extend({
 
-		template: _.template(pluginConfigTemplate),
+    template: _.template(pluginConfigTemplate),
 
-		initialize: function(options) {
-			this.required = this.model && this.model.constraints ? this.model.constraints.required : false;
-			this.value = options.value;
-		},
+    initialize: function (options) {
+      this.required = this.model && this.model.constraints ? this.model.constraints.required : false;
+      this.value = options.value;
+    },
 
-		render: function() {
-			this.$el.html(this.template({
-				strings: strings,
-				id: this.getSafeId(),
-				property: this.model,
-				required: this.required,
-				value: this.value
-			}));
+    render: function () {
+      this.$el.html(this.template({
+        strings: strings,
+        id: this.getSafeId(),
+        property: this.model,
+        required: this.required,
+        value: this.value
+      }));
 
-			// load presence entities
-			HubService.getPresenceEntities(this, function(model, response, options) {
-				var el = options.context.$el.find('#' + options.context.getSafeId());
-				el.empty();
-				for (var ix=0; ix < model.length; ix++) {
-					el.append('<option value="' + model.at(ix).get('@id') + '">' + model.at(ix).get('name') + '</option>');
-				}
-				el.removeAttr('disabled');
-			}, function(model, response, options) {
-				toastr.error('Error retrieving location list');
-			});
+      // load presence entities
+      HubService.getPresenceEntities(this, function (model, response, options) {
+        var el = options.context.$el.find('#' + options.context.getSafeId());
+        el.empty();
+        for (var ix = 0; ix < model.length; ix++) {
+          el.append('<option value="' + model.at(ix).get('@id') + '">' + model.at(ix).get('name') + '</option>');
+        }
+        el.removeAttr('disabled');
+      }, function (model, response, options) {
+        toastr.error('Error retrieving location list');
+      });
 
-			return this;
-		},
+      return this;
+    },
 
-	    getValue: function() {
-	    	var id = this.$el.find('#' + this.getSafeId()).val();
-	    	return {'@id': id, name: this.$el.find('option[value="' + id + '"]').text()};
-	    }
+    getValue: function () {
+      var id = this.$el.find('#' + this.getSafeId()).val();
+      if (id) {
+        return {'@id': id, name: this.$el.find('option[value="' + id + '"]').text()};
+      } else {
+        return null;
+      }
+    },
 
-	});
+    showError: function (showError) {
+      BaseWidget.prototype.showError.call(this, showError);
+      if (showError) {
+        this.$el.find('#person').addClass('error');
+      } else {
+        this.$el.find('#person').removeClass('error');
+      }
+    }
+
+  });
 
 });
