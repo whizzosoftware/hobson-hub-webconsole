b537054985b84a6e635d83b9ee191e95d12c6443
Daniel Noguerol <dan@whizzosoftware.com>
2016-08-23 07:55:00 -0600

    Fixed problem with task tile clicking.

--- yo/app/js/hub/views/collection/task.js
+++ yo/app/js/hub/views/collection/task.js
@@ -1,57 +1,60 @@
 // Filename: views/collection/task.js
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'moment',
-	'i18n!nls/strings',
-	'text!templates/collection/task.html'
-], function($, _, Backbone, moment, strings, template) {
+  'jquery',
+  'underscore',
+  'backbone',
+  'moment',
+  'i18n!nls/strings',
+  'text!templates/collection/task.html'
+], function ($, _, Backbone, moment, strings, template) {
 
-	return Backbone.View.extend({
+  return Backbone.View.extend({
 
-		tagName: 'li',
+    tagName: 'li',
 
-		template: _.template(template),
+    template: _.template(template),
 
-		events: {
+    events: {
       'click': 'onClick',
-			'click #delete-task': 'onClickDelete'
-		},
-
-		render: function() {
-			this.$el.append(
-				this.template({
-					strings: strings,
-					task: this.model.toJSON(),
-					schedule: this.createSchedule(this.model)
-				})
-			);
-
-			return this;
-		},
-
-    onClick: function() {
-		  this.$el.trigger('executeTask', this.model);
+      'click #delete-task': 'onClickDelete'
     },
 
-		onClickDelete: function() {
-			this.$el.trigger('deleteTask', this.model);
-		},
+    render: function () {
+      this.$el.append(
+        this.template({
+          strings: strings,
+          task: this.model.toJSON(),
+          schedule: this.createSchedule(this.model)
+        })
+      );
 
-		createSchedule: function(task) {
-			var now = moment();
-			var schedule = {};
-			if (task.get('properties') && task.get('properties').scheduled) {
-				schedule.active = true;
-				var nextRun = new moment(task.get('properties').nextRunTime);
-				if (!nextRun.isBefore(now)) {
-					schedule.nextRunStr = strings.NextRun + ' ' + nextRun.fromNow();
-				}
-			}
-			return schedule;
-		}
+      return this;
+    },
 
-	});
+    onClick: function (e) {
+      if (e.toElement.className.indexOf('task-tile') > -1) {
+        this.$el.trigger('executeTask', this.model);
+      }
+    },
+
+    onClickDelete: function (e) {
+      e.preventDefault();
+      this.$el.trigger('deleteTask', this.model);
+    },
+
+    createSchedule: function (task) {
+      var now = moment();
+      var schedule = {};
+      if (task.get('properties') && task.get('properties').scheduled) {
+        schedule.active = true;
+        var nextRun = new moment(task.get('properties').nextRunTime);
+        if (!nextRun.isBefore(now)) {
+          schedule.nextRunStr = strings.NextRun + ' ' + nextRun.fromNow();
+        }
+      }
+      return schedule;
+    }
+
+  });
 
 });
